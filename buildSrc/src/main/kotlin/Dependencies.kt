1db2c3289e2bd6cdabe323455eea42f062581d33
Joey Harward <joey@metalpay.co>
2021-02-04 20:43:14 -0500

    using to jdk8 for kotlin std lib added lint ignores

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -74,7 +74,7 @@ object Libraries {
 		const val esr = "1.0.6"
 	}
 
-	const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion"
+	const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinVersion"
 	const val ktxCore = "androidx.core:core-ktx:${Versions.ktx}"
 	const val lifeCycleLiveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycleLiveData}"
 	const val roomKtx = "androidx.room:room-ktx:${Versions.room}"
